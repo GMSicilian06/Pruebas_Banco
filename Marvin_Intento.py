739989e77551d9068e866d5895cca369e43f9614
Marvin Vallecillo <31805306+valldriz@users.noreply.github.com>
2026-07-17 15:22:55 -0600

    Refactor XML reading and update main processing logic
    Refactor XML reading and file handling functions. Update main function to process files from a specified directory and save results to an Excel file.

--- Marvin_Intento.py
+++ Marvin_Intento.py
@@ -1,46 +1,97 @@
+from lxml import etree
 import pandas as pd
+import os
 from pathlib import Path
 from datetime import datetime
 
-# df = pd.read_excel('/Users/carlossequeira/Downloads/Estudiantes(Público).xlsx')
+def leer_xml(file_path):
+    #tree = etree.parse(r"\\172.25.16.15\Reportes Cobis_BI\Remesas\2026\Julio\16072026\remesasdi_RIA_1616072026.xml")
+    tree = etree.parse(file_path)
 
+    ns = {
+        "ss": "urn:schemas-microsoft-com:office:spreadsheet"
+    }
 
-#info importante, modificar antes de ejecutar
+    rows = []
 
-#ver si es una fecha válida
+    for row in tree.xpath("//ss:Row", namespaces=ns):
+        values = []
+
+        for cell in row.xpath("./ss:Cell", namespaces=ns):
+            data = cell.xpath("./ss:Data/text()", namespaces=ns)
+
+            values.append(data[0] if data else "")
+
+        rows.append(values)
+
+    df = pd.DataFrame(rows)
+    return df
+
+def read_file_to_dataframe(file_path):
+    """
+    Leer los archivos
+    """
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    # Get file extension in lowercase
+    ext = os.path.splitext(file_path)[1].lower()
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    try:
+        if ext in ['.xml']:
+           df = leer_xml(file_path)
+           
+        elif ext in ['.xls', '.xlsx']:
+            """try:
+               df = pd.read_csv(file_path, sep= "\t")
+            except Exception as e:"""
+            df = pd.read_csv(file_path, header=None)
+                
+        else:
+            raise ValueError(f"Unsupported file type: {ext}")
+        
+        return df
+
+    except Exception as e:
+        raise ValueError(f"Error reading {file_path}: {e}")
+    
 def is_valid_date(date_string, date_format="%d%m%Y"):
     try:
         datetime.strptime(date_string, date_format)
         return True
     except ValueError:
         return False
-        
-folder_path = Path("path/to/your/folder")
-
-df_PAGADAS= pd.DataFrame()
-df_ENVIADAS= pd.DataFrame()
 
 #separar los excels según los nombres en las filas
 def encontrar_fila_separadora(df: pd.DataFrame, marker1: str, marker2: str):
     marker1_idx = None
     marker2_idx = None
- 
+
+    print(df)
+    print("")
+    print("")
+    print("")
+
     for idx, row in df.iterrows():
+        print(idx, row.loc[0])
         row_text = " ".join(str(v) for v in row.values if pd.notna(v))
         if marker1_idx is None and marker1 in row_text:
             marker1_idx = idx
         elif marker2_idx is None and marker2 in row_text:
             marker2_idx = idx
- 
+
     if marker1_idx is None or marker2_idx is None:
-        raise ValueError(
+        print(
             f"No encuentra los separadores o tal vez no tienen los mismos nombres"
             f"no se encontró el primer separador "
             f"no se encontró el segundo separador"
         )
- 
+
     return marker1_idx, marker2_idx
- 
+
 def dividir_dataframe(df: pd.DataFrame, marker1_idx: int, marker2_idx: int):
     """
     Divide la data en dos bloques 
@@ -50,49 +101,34 @@ def dividir_dataframe(df: pd.DataFrame, marker1_idx: int, marker2_idx: int):
     df1 = df.iloc[header1_idx + 1 : marker2_idx].copy()
     df1.columns = df.iloc[header1_idx]
     df1 = df1.reset_index(drop=True)
- 
+
     #corre hasta el final
     header2_idx = marker2_idx + 1
     df2 = df.iloc[header2_idx + 1 :].copy()
     df2.columns = df.iloc[header2_idx]
     df2 = df2.reset_index(drop=True)
- 
+
     return df1, df2
 
-def main(fecha):
-	for item in folder_path.iterdir():
-		nueva_fecha = [item.name[i:i+2] for i in range(len(item.name) - 2)]
-		if item.is_dir() and is_valid_date(item.name) and int(nueva_fecha[0]) <= int(fecha):
-			for documento in item.iterdir():
-				if documento.name.startswith('remesasdi'):
-					df_remesas_di = pd.read_excel(documento)
-					del df_remesas_di['MTO_PAGADOS_CORDOBAS']
+def main(fecha, file_path):
+    folder_path = Path(file_path)
+    df_PAGADAS = pd.DataFrame()
+    df_ENVIADAS = pd.DataFrame()
 
-					marker1, marker2 = encontrar_fila_separadora(df_remesas_di, "REMESAS PAGADAS", 'REMESAS ENVIADAS')
-					df1, df2 = dividir_dataframe(df_remesas_di, marker1, marker2)
+    for item in folder_path.iterdir():
+        nueva_fecha = [item.name[i:i+2] for i in range(len(item.name) - 2)]
+        if item.is_dir() and is_valid_date(item.name) and int(nueva_fecha[0]) <= int(fecha):
+            for documento in item.iterdir():
+                print(documento)
+                df = read_file_to_dataframe(documento)
+                marker__1, marker__2 = encontrar_fila_separadora(df, "REMESAS PAGADAS", 'REMESAS ENVIADAS')
+                df__1, df__2 = dividir_dataframe(df, marker__1, marker__2)
+                df_PAGADAS = pd.concat([df_PAGADAS, df__1], ignore_index=True)
+                df_ENVIADAS = pd.concat([df_ENVIADAS, df__2], ignore_index=True)
 
-					df_PAGADAS = pd.concat([df_PAGADAS, df1], ignore_index=True)
-					df_ENVIADAS = pd.concat([df_ENVIADAS, df2], ignore_index=True)
+    with pd.ExcelWriter(r"C:\Users\NI38504\Documents\Home\REMESAS\PRUEBAS-PY\REMESAS_ENVIADAS_PAGADAS.xlsx") as writer:
+        df_PAGADAS.to_excel(writer, sheet_name='PAGADAS', index=False)
+        df_ENVIADAS.to_excel(writer, sheet_name='ENVIADAS', index=False)
 
-				elif documento.name.startswith('transnetwork_di'):
-					df_transnetwork_di = pd.read_excel(documento)
-					del df_transnetwork_di['MTO_PAGADOS_CORDOBAS']
-
-					marker_1, marker_2 = encontrar_fila_separadora(df_transnetwork_di, "REMESAS PAGADAS", 'REMESAS ENVIADAS')
-					df_1, df_2 = dividir_dataframe(df_transnetwork_di, marker_1, marker_2)
-
-					df_PAGADAS = pd.concat([df_PAGADAS, df_1], ignore_index=True)
-					df_ENVIADAS = pd.concat([df_ENVIADAS, df_2], ignore_index=True)
-
-				elif documento.name.startswith('pairpack_di'):
-					df_pairpack_di = pd.read_excel(documento)
-
-					marker__1, marker__2 = encontrar_fila_separadora(df_pairpack_di, "REMESAS PAGADAS", 'REMESAS ENVIADAS')
-					df__1, df__2 = dividir_dataframe(df_pairpack_di, marker__1, marker__2)
-
-					df_PAGADAS = pd.concat([df_PAGADAS, df__1], ignore_index=True)
-					df_ENVIADAS = pd.concat([df_ENVIADAS, df__2], ignore_index=True)
-
-with pd.ExcelWriter('REMESAS_ENVIADAS_PAGADAS.xlsx') as writer:
-    df_PAGADAS.to_excel(writer, sheet_name='PAGADAS', index=False)
-    df_ENVIADAS.to_excel(writer, sheet_name='ENVIADAS', index=False)
+if __name__ == "__main__":
+    main(20260701, r"\\172.25.16.15\Reportes Cobis_BI\Remesas\2026\Julio")
